304de500f09844880ee35ebd2db8676495e67b39
Marwahkamilaahmad <Mymarwahmad@gmail.com>
2024-05-11 23:48:21 +0700

    first

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,24 @@
 
-export default function Navbar() {
-  return (
-    <div>Navbar</div>
+
+const Navbar = () => {
+  return(
+    <div className="n-wrapper">
+      <div className="n-left">
+        <div className="n name"></div>
+        <span>toggle</span>
+      </div>
+      <div className="n-right">
+        <div className="n-list">
+          <ul>
+            <li>Home</li>
+            <li>Services</li>
+            <li>Experience</li>
+          </ul>
+        </div>
+      </div>
+    </div>
   )
 }
+
+export default Navbar;
+
