9945a5e28e94367109b6f2913fb33fd702572389
Marwahkamilaahmad <Mymarwahmad@gmail.com>
2024-05-12 10:17:33 +0700

    5th

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,52 +1,52 @@
+import "./Navbar.css";
 
-// import './Navbar.css';
-
-// const Navbar = () => {
-//   return (
-//     <div className="navbar-wrapper">
-//       <div className="navbar-left">
-//         <div className="navbar-name">Logo</div>
-//         <span>toggle</span>
-//       </div>
-//       <div className="navbar-right">
-//         <div className="navbar-list">
-//           <ul>
-//             <li>Home</li>
-//             <li>Services</li>
-//             <li>Experience</li>
-//           </ul>
-//         </div>
-//       </div>
-//     </div>
-//   );
-
-// }
-
-
-// export default Navbar;
 
 import { Link } from "react-router-dom";
+import toggle_dark from "../assets/night.png";
+import toggle_light from "../assets/day.png";
 
 const Navbar = () => {
   return (
-    <div className="flex justify-between items-center bg-gray-900 text-white p-4">
-      <div className="flex items-center">
+    <div className="navbar-wrapper">
+      <div className="navbar-left">
         <div className="mr-4">Logo</div>
-        <span>toggle</span>
       </div>
-      <div className="hidden md:block">
+
+      <div className="navbar-right">
         <ul className="flex">
-          <li className="mr-4"><Link to="/about">about</Link></li>
-          <li className="mr-4"><Link to="/projects">projects</Link></li>
-          <li className="mr-4"><Link to="technologies">technologies</Link></li>
+          <li className="sub-list">
+            <Link
+              to="/about"
+              style={{ textDecoration: "none", color: "inherit" }}
+            >
+              about
+            </Link>
+          </li>
+          <li className="sub-list">
+            <Link
+              to="/projects"
+              style={{ textDecoration: "none", color: "inherit" }}
+            >
+              projects
+            </Link>
+          </li>
+          <li className="sub-list">
+            <Link
+              to="technologies"
+              style={{ textDecoration: "none", color: "inherit" }}
+            >
+              technologies
+            </Link>
+          </li>
+          <li>
+            <div className="toggle-icon">
+              <img src={toggle_dark} alt="" className="toggle-img" />
+            </div>
+          </li>
         </ul>
       </div>
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
     </div>
   );
-}
+};
 
 export default Navbar;
-
