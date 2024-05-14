324a5c7cd5b210b96ac8817bae144b0802ab8f10
Marwahkamilaahmad <Mymarwahmad@gmail.com>
2024-05-14 12:50:40 +0700

    7th

--- src/App.jsx
+++ src/App.jsx
@@ -3,24 +3,29 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import General from "./pages/General";
+import Home from "./pages/Home";
+import { ChakraProvider } from "@chakra-ui/react";
 
 function App() {
   const [theme, setTheme] = useState("light");
 
   return (
-    <Router>
-      <div className={`container ${theme}`}>
-        <Navbar theme={theme} setTheme={setTheme} />
-        <General theme={theme} setTheme={setTheme}/>
-      </div>
-      <Routes>
-        <Route path="/" element={""} />
-        <Route path="/about" element={""} />
-        <Route path="/projects" element={""} />
-        <Route path="/" element={""} />
-        <Route path="/technologies" element={""} />
-      </Routes>
-    </Router>
+    <ChakraProvider>
+      <Router>
+        <div className={`container ${theme}`}>
+          <Navbar theme={theme} setTheme={setTheme} />
+          <General theme={theme} setTheme={setTheme} />
+          <Home />
+        </div>
+        <Routes>
+          <Route path="/" element={""} />
+          <Route path="/about" element={""} />
+          <Route path="/projects" element={""} />
+          <Route path="/" element={""} />
+          <Route path="/technologies" element={""} />
+        </Routes>
+      </Router>
+    </ChakraProvider>
   );
 }
 
