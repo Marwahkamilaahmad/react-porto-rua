6b1fa3e52988eb19f72032d34fb425c83c144832
Marwahkamilaahmad <Mymarwahmad@gmail.com>
2024-05-19 09:19:41 +0700

    add  timeline

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,18 @@
+import EducationTimeline from "../components/EducationTimeline";
 import Techstack from "../components/Techstack";
-
 
 export default function Home() {
   return (
     <main className="container mx-auto max-width text-center">
       <section>
         <h4 className="text-dark-heading dark:text-light-heading xl:text-3xl xl:leading-tight font-bold">
-          Education & Timeline
-        </h4>
-        <h4 className="text-dark-heading dark:text-light-heading xl:text-3xl xl:leading-tight font-bold">
           Tech Stack
         </h4>
         <Techstack />
+        <h4 className="text-dark-heading dark:text-light-heading xl:text-3xl xl:leading-tight font-bold">
+          Education & Timeline
+        </h4>
+        <EducationTimeline />
       </section>
     </main>
   );
