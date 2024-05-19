7e3b1d6b873654a9dd789fb4048ac230c7cd1ea5
Marwahkamilaahmad <Mymarwahmad@gmail.com>
2024-05-19 17:31:21 +0700

    add LanguageSkill

---
+++ src/components/LanguageSkill.jsx
@@ -0,0 +1,36 @@
+export default function LanguageSkill() {
+  return (
+    <section>
+      <div className="stats shadow m-3">
+        <div className="stat">
+          <div className="stat-title">Advance</div>
+          <div className="stat-value">Bahasa</div>
+        </div>
+      </div>
+      <div className="stats shadow m-3">
+        <div className="stat">
+          <div className="stat-title">Advance</div>
+          <div className="stat-value">English</div>
+        </div>
+      </div>
+      <div className="stats shadow m-3">
+        <div className="stat">
+          <div className="stat-title">Advance</div>
+          <div className="stat-value">Korea</div>
+        </div>
+      </div>
+      <div className="stats shadow m-3">
+        <div className="stat">
+          <div className="stat-title">Advance</div>
+          <div className="stat-value">Chinese</div>
+        </div>
+      </div>
+      <div className="stats shadow m-3">
+        <div className="stat">
+          <div className="stat-title">Advance</div>
+          <div className="stat-value">Arab</div>
+        </div>
+      </div>
+    </section>
+  );
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import EducationTimeline from "../components/EducationTimeline";
+import LanguageSkill from "../components/LanguageSkill";
 import Techstack from "../components/Techstack";
 
 export default function Home() {
@@ -9,7 +10,11 @@ export default function Home() {
           Tech Stack
         </h4>
         <Techstack />
-        <h4 className="text-dark-heading dark:text-light-heading xl:text-3xl xl:leading-tight font-bold">
+        <h4 className="text-dark-heading mt-10 mb-10 dark:text-light-heading xl:text-3xl xl:leading-tight font-bold">
+          Language Proficiency
+        </h4>
+        <LanguageSkill />
+        <h4 className="text-dark-heading mt-10 mb-10 dark:text-light-heading xl:text-3xl xl:leading-tight font-bold">
           Education & Timeline
         </h4>
         <EducationTimeline />
