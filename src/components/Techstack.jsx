902fa4900743bc5fac57e676396e4c8ee72629e8
Marwahkamilaahmad <Mymarwahmad@gmail.com>
2024-05-19 08:55:06 +0700

    5th

--- src/components/Techstack.jsx
+++ src/components/Techstack.jsx
@@ -1,34 +1,130 @@
-import {
-  Card,
-  CardHeader,
-  CardBody,
-  CardFooter,
-  Heading,
-  Button,
-  Text,
-} from "@chakra-ui/react";
+import githubicons from "../assets/github-icons.png";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
 
 export default function Techstack() {
+  useEffect(() => {
+    AOS.init({ duration: 1000 });
+  }, []);
+
   return (
-    <div className="my-12 mx-auto px-4 md:px-12 justify-center">
-      <div className="rounded-lg shadow-lg bg-white">
-        <header className="flex items-center justify-between leading-tight p-2 md:p-4">
-          <h1 className="text-lg">
-            <a className="no-underline hover:underline text-black" href="#">
-              Article Title
-            </a>
-          </h1>
-        </header>
-        <div className="flex items-center md:p-4">
-          <p className="text-sm">Author Name</p>
-          <a
-            className="no-underline text-grey-darker hover:text-red-dark"
-            href="#"
-          >
-            <span className="hidden">Like</span>
-            <i className="fa fa-heart"></i>
-          </a>
-        </div>
+    <div className="mt-8">
+      <div className="flex item-center justify-center flex-wrap" data-aos="fade-left">
+        <img
+          src={githubicons}
+          alt=""
+          className="h-20 w-20 rounded-full p-1 m-3"
+        />
+        <img
+          src={githubicons}
+          alt=""
+          className="h-20 w-20 rounded-full p-1 m-3"
+        />
+        <img
+          src={githubicons}
+          alt=""
+          className="h-20 w-20 rounded-full p-1 m-3"
+        />
+        <img
+          src={githubicons}
+          alt=""
+          className="h-20 w-20 rounded-full p-1 m-3"
+        />
+        <img
+          src={githubicons}
+          alt=""
+          className="h-20 w-20 rounded-full p-1 m-3"
+        />
+        <img
+          src={githubicons}
+          alt=""
+          className="h-20 w-20 rounded-full p-1 m-3"
+        />
+        <img
+          src={githubicons}
+          alt=""
+          className="h-20 w-20 rounded-full p-1 m-3"
+        />
+        <img
+          src={githubicons}
+          alt=""
+          className="h-20 w-20 rounded-full p-1 m-3"
+        />
+      </div>
+      <div className="flex item-center justify-center flex-wrap" data-aos="fade-right">
+        <img
+          src={githubicons}
+          alt=""
+          className="h-20 w-20 rounded-full p-1 m-3"
+        />
+        <img
+          src={githubicons}
+          alt=""
+          className="h-20 w-20 rounded-full p-1 m-3"
+        />
+        <img
+          src={githubicons}
+          alt=""
+          className="h-20 w-20 rounded-full p-1 m-3"
+        />
+        <img
+          src={githubicons}
+          alt=""
+          className="h-20 w-20 rounded-full p-1 m-3"
+        />
+        <img
+          src={githubicons}
+          alt=""
+          className="h-20 w-20 rounded-full p-1 m-3"
+        />
+        <img
+          src={githubicons}
+          alt=""
+          className="h-20 w-20 rounded-full p-1 m-3"
+        />
+      </div>
+      <div className="flex item-center justify-center flex-wrap" data-aos="fade-left">
+        <img
+          src={githubicons}
+          alt=""
+          className="h-20 w-20 rounded-full p-1 m-3"
+        />
+        <img
+          src={githubicons}
+          alt=""
+          className="h-20 w-20 rounded-full p-1 m-3"
+        />
+        <img
+          src={githubicons}
+          alt=""
+          className="h-20 w-20 rounded-full p-1 m-3"
+        />
+        <img
+          src={githubicons}
+          alt=""
+          className="h-20 w-20 rounded-full p-1 m-3"
+        />
+        <img
+          src={githubicons}
+          alt=""
+          className="h-20 w-20 rounded-full p-1 m-3"
+        />
+        <img
+          src={githubicons}
+          alt=""
+          className="h-20 w-20 rounded-full p-1 m-3"
+        />
+        <img
+          src={githubicons}
+          alt=""
+          className="h-20 w-20 rounded-full p-1 m-3"
+        />
+        <img
+          src={githubicons}
+          alt=""
+          className="h-20 w-20 rounded-full p-1 m-3"
+        />
       </div>
     </div>
   );
